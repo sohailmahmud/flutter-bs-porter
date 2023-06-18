25c84c72500ad80698a0d33651120e36dba96d58
ProHealth <prohealtheng@gmail.com>
2023-06-18 15:55:59 +0600

    bs fix

---
+++ README.md
@@ -0,0 +1,17 @@
+# flutter_bs_porter
+
+A new Flutter project.
+
+## App features
+This is simple Flutter app that when it's opened it will show a screen with a counter that it's incremented every second. The interesting thing is that the counter never stops counting. No matter if you remove the app from recent apps or Android system kills the process to reclaim more memory. The app always recovers from any situation and keeps counting. Just uninstall the app if you want to stop it.
+
+These are the techniques used by this project to keep Flutter running:
+
+Use of moveTaskToBack to survive when back button is pressed.
+Creation of a foreground service to increase process priority.
+Start and stop a new Isolate to run Dart background code when FlutterActivity is destroyed.
+
+## How to run the project
+Follow Get Started tutorial and install Flutter
+Start and Android emulator
+Run the app from terminal with flutter run
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bs_porter/app_retain_widget.dart';
+import 'package:flutter_bs_porter/background_main.dart';
+import 'package:flutter_bs_porter/counter_service.dart';
+
+void main() {
+  runApp(MyApp());
+
+  var channel = const MethodChannel('com.example/background_service');
+  var callbackHandle = PluginUtilities.getCallbackHandle(backgroundMain);
+  channel.invokeMethod('startService', callbackHandle.toRawHandle());
+
+  CounterService.instance().startCounting();
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Background Demo',
+      home: AppRetainWidget(
+        child: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Background Demo'),
+      ),
+      body: Center(
+        child: ValueListenableBuilder(
+          valueListenable: CounterService.instance().count,
+          builder: (context, count, child) {
+            return Text('Counting: $count');
+          },
+        ),
+      ),
+    );
+  }
+}
